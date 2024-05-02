119584fbdd60c38e5de80935458870652e1c630f
TerryHenrar <henrardterry2203@hotmail.com>
2024-05-02 16:48:10 +0200

    TERRY: first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OfferVerse.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
