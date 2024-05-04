e800153053bcbabcf27c95da209fd15080b6c75a
Dimitri <maraindimitri@gmail.com>
2024-05-04 17:19:26 +0200

    DIMITRI: create list of provided services

---
+++ Controllers/MyServicesController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OfferVerse.DAL.Interfaces;
+using System.Reflection.Metadata.Ecma335;
+using AppUser = OfferVerse.Models.User;
+
+namespace OfferVerse.Controllers
+{
+    public class MyServicesController : Controller
+    {
+        private readonly IUserDAL dal;
+        public MyServicesController(IUserDAL dal)
+        {
+            this.dal = dal;
+        }
+        public IActionResult MyServices()
+        {
+            return View(AppUser.GetAllServicesProvided(dal, 4));
+        }
+    }
+}
