47ca13d90a5a2635283f086dfa3f3bdf24cb0a40
TerryHenrar <henrardterry2203@hotmail.com>
2024-05-15 15:34:11 +0200

    TERRY: add link in _layout

--- obj/Debug/net6.0/OfferVerse.AssemblyInfo.cs
+++ obj/Debug/net6.0/OfferVerse.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OfferVerse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+903583e068a4bcaeabbf10def35321336d5c2384")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+677a754a919cbd2b4905921648313de6b104809c")]
 [assembly: System.Reflection.AssemblyProductAttribute("OfferVerse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OfferVerse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
