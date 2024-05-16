0614aca18b351046a00c85be0aa5b06256e7a0e2
TerryHenrar <henrardterry2203@hotmail.com>
2024-05-16 16:02:03 +0200

    TERRY: upgrade the general appearence

--- obj/Debug/net6.0/OfferVerse.AssemblyInfo.cs
+++ obj/Debug/net6.0/OfferVerse.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OfferVerse")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8778de7be5efa33ac3c59bdaa3079a15607b665")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff4baeb30659920ebdcc0ce203b17137023dad46")]
 [assembly: System.Reflection.AssemblyProductAttribute("OfferVerse")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OfferVerse")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
